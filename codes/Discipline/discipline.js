e9437d5b1fd0aa5eee808c190ab2b779c30be81b
masaki <ohashimasaki@gmail.com>
2018-07-12 13:53:52 +0900

    new

---
+++ codes/Discipline/discipline.js
@@ -0,0 +1,293 @@
+﻿function Discipline(rules) {
+
+    this.constrain = function() {
+        for(var name in rules.default) {
+            var props = rules.default[name];
+            var elements = find(name);
+            change(elements, props, null);
+        }
+        for(var id in rules) {
+            if(id != "default") {
+                var rule = rules[id];
+                if(rule.when && rule.then) {
+                    for(var name in rule.when) {
+                         attach(name, id);
+                     }
+                }
+            }
+        }
+    }
+
+
+    //----------------------------------------------------------------------------------------------------
+    function attach(name, id) {
+
+        var e = document.getElementById(name);
+        if(e) {
+            if( ! e.disciplineAttached) {
+                e.addEventListener("change", function() {
+                    affect(this);
+                }, false);
+                e.disciplineAttached = true;
+            }
+            if( ! e.disciplineRules) {
+                e.disciplineRules = [];
+            }
+            if(e.disciplineRules.indexOf(id) < 0) {
+                e.disciplineRules.push(id);
+            }
+            return;
+        }
+
+        var q = document.querySelectorAll("input[name='" + name + "']");
+        for(var i = 0; i < q.length; i++) {
+            var e = q[i];
+            if(/^radio$/i.test(e.type)) {
+                if( ! e.disciplineAttached) {
+                    e.addEventListener("change", function() {
+                        affect(this);
+                    }, false);
+                    e.disciplineAttached = true;
+                }
+                if( ! e.disciplineRules) {
+                    e.disciplineRules = [];
+                }
+                if(e.disciplineRules.indexOf(id) < 0) {
+                    e.disciplineRules.push(id);
+                }
+            }
+        }
+
+    }
+    //----------------------------------------------------------------------------------------------------
+    function affect(e) {
+
+        var matched = [];
+        var unmatched = [];
+
+        for(var i = 0; i < e.disciplineRules.length; i++) {
+            var id = e.disciplineRules[i];
+            var rule = rules[id];
+            if(evaluate(rule.when)) {
+                matched.push(id);
+            } else {
+                unmatched.push(id);
+            }
+        }
+
+        reset(matched, unmatched);
+
+        for(var i = 0; i < matched.length; i++) {
+            var id = matched[i];
+            var rule = rules[id];
+            for(var name in rule.then) {
+                var props = rule.then[name];
+                var elements = find(name);
+                change(elements, props, e);
+            }
+        }
+
+    }
+    //----------------------------------------------------------------------------------------------------
+    function reset(matched, unmatched) {
+
+        var items = {};
+
+        for(var i = 0; i < unmatched.length; i++) {
+            var id = unmatched[i];
+            var rule = rules[id];
+            for(var name in rule.then) {
+                if(name in rules.default) {
+                    items[name] = {};
+                    var props = rules.default[name];
+                    for(var prop in props) {
+                        items[name][prop] = rules.default[name][prop];
+                    }
+                }
+            }
+        }
+
+        for(var i = 0; i < matched.length; i++) {
+            var id = matched[i];
+            var rule = rules[id];
+            for(var name in rule.then) {
+                delete items[name];
+            }
+        }
+
+        for(var name in items) {
+            var props = items[name];
+            var elements = find(name);
+            change(elements, props, null);
+        }
+
+    }
+    //----------------------------------------------------------------------------------------------------
+    function change(elements, props, src) {
+
+        for(var p in props) {
+            if(typeof props[p] == "function") {
+                var f = props[p];
+                for(var i = 0; i < elements.length; i++) {
+                    var e = elements[i];
+                    if(p in e) {
+                        e[p] = f(src, e);
+                    } else if(p in e.style) {
+                        e.style[p] = f(src, e);
+                    }
+                }
+            } else {
+                var value = props[p];
+                for(var i = 0; i < elements.length; i++) {
+                    var e = elements[i];
+                    if(p in e) {
+                        if(/^select$/i.test(e.tagName) && e.getAttribute("cascade")) {
+                            e[p] = value;
+                            react(e);
+                        } else if(/^input$/i.test(e.tagName) && /^radio$/i.test(e.type) && /^value$/i.test(p)) {
+                            if(e.value == value) {
+                                e.checked = true;
+                            }
+                        } else {
+                            e[p] = value;
+                        }
+                    } else if(p in e.style) {
+                        e.style[p] = value;
+                    }
+                }
+            }
+        }
+
+    }
+    //----------------------------------------------------------------------------------------------------
+    function evaluate(rule) {
+
+        for(var name in rule) {
+            var e = document.getElementById(name) || document.querySelector("input[name='" + name + "']:checked");
+            if( ! e) {
+                return false;
+            } else {
+                var props = rule[name];
+                for(var prop in props) {
+                    var v = props[prop];
+
+                    if(/^(?:=|[!<>]=?)/.test(v)) {
+                        var q = /^(=|[!<>]=?)(.*)$/.exec(v);
+                        var op = (q == null) ? "=" : (q[1] || "=");
+                        var value = (q == null) ? v : (q[2] || "");
+                        value = /^(?:true|false)$/i.test(value) ? /^true$/i.test(value) : value;
+                    } else {
+                        var op = "=";
+                        var value = trim(v);
+                    }
+
+                    if(prop in e && ! compare(e[prop], value, op)) {
+                        return false;
+                    } else if(prop in e.style && ! compare(e.style[prop], value, op)) {
+                        return false;
+                    } else if( ! (prop in e || prop in e.style)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+
+    }
+    //----------------------------------------------------------------------------------------------------
+    function compare(a, b, op) {
+
+        if(op == "=" && a == b) {
+            return true;
+        } else if(op == "!" && a != b) {
+            return true;
+        } else if(op == "!=" && a != b) {
+            return true;
+        } else if(op == "<" && a < b) {
+            return true;
+        } else if(op == "<=" && a <= b) {
+            return true;
+        } else if(op == ">" && a > b) {
+            return true;
+        } else if(op == ">=" && a >= b) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    //----------------------------------------------------------------------------------------------------
+    function find(name) {
+
+        var q = [];
+
+        var e = document.getElementById(name);
+        if(e) {
+            q.push(e);
+        }
+
+        var e = document.querySelectorAll("input[name='" + name + "']");
+        for(var i = 0; i < e.length; i++) {
+            if(/^radio$/i.test(e[i].type)) {
+                q.push(e[i]);
+            }
+        }
+
+        return q;
+
+    }
+    //----------------------------------------------------------------------------------------------------
+    function trim(t) {
+
+        if(t == null || typeof t != "string") {
+            return t;
+        }
+
+        return t.replace(/^\s+|\s+$/g, "");
+
+    }
+    //----------------------------------------------------------------------------------------------------
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ codes/Discipline/rules.js
@@ -0,0 +1,182 @@
+﻿var rules = {
+"default": {
+    "radio": {
+        "value": "A1"
+    },
+    "checkbox1": {
+        "disabled": true,
+        "checked": false
+    },
+    "checkbox2": {
+        "disabled": true,
+        "checked": false
+    },
+    "checkbox3": {
+        "disabled": true,
+        "checked": false
+    },
+    "textbox1": {
+        "disabled": true,
+        "value": "",
+        "background-color": ""
+    },
+    "textbox2": {
+        "disabled": true,
+        "value": "",
+        "background-color": ""
+    },
+    "textbox3": {
+        "disabled": true,
+        "value": "",
+        "background-color": ""
+    }
+},
+"#1": {
+    "when": {
+        "radio": {
+            "value": "A1"
+        },
+        "select1": {
+            "value": ">=2"
+        }
+    },
+    "then": {
+        "textbox1": {
+            "disabled": false,
+            "value": ""
+        }
+    }
+},
+"#2": {
+    "when": {
+        "select1": {
+            "value": "1"
+        }
+    },
+    "then": {
+        "textbox2": {
+            "disabled": false,
+            "value": "\"some\/ \ntext\"\n he\\re"
+        }
+    }
+},
+"#3": {
+    "when": {
+        "radio": {
+            "value": "A2"
+        }
+    },
+    "then": {
+        "checkbox1": {
+            "disabled": false
+        },
+        "checkbox2": {
+            "disabled": false
+        },
+        "checkbox3": {
+            "disabled": false
+        },
+        "textbox3": {
+            "disabled": false,
+            "value": function(src, self) { return !self.value ? "hey" : self.value; },
+            "background-color": "#ffff00"
+        },
+        "select1": {
+            "value": "2"
+        },
+        "select5": {
+            "value": "1"
+        }
+    }
+},
+"#4": {
+    "when": {
+        "radio": {
+            "value": "A3"
+        }
+    },
+    "then": {
+        "select1": {
+            "value": "3"
+        },
+        "select5": {
+            "value": "2"
+        }
+    }
+},
+"#5": {
+    "when": {
+        "radio": {
+            "value": "A2"
+        },
+        "checkbox1": {
+            "checked": true
+        },
+        "checkbox2": {
+            "checked": true
+        },
+        "checkbox3": {
+            "checked": true
+        }
+    },
+    "then": {
+        "textbox2": {
+            "disabled": false,
+            "background-color": "#ff00ff"
+        },
+        "select1": {
+            "value": "3"
+        }
+    }
+},
+"#6": {
+    "when": {
+        "textbox3": {
+            "disabled": false,
+            "value": ""
+        }
+    },
+    "then": {
+        "textbox3": {
+            "background-color": "#ff0000"
+        }
+    }
+},
+"#7": {
+    "when": {
+        "textbox3": {
+            "disabled": false,
+            "value": "!="
+        }
+    },
+    "then": {
+        "textbox3": {
+            "background-color": "#00ffff"
+        }
+    }
+}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
