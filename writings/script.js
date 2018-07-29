f2d60631e14f68b58a8d2152d0e8535287e2675e
masaki <ohashimasaki@gmail.com>
2018-07-30 03:48:58 +0900

    fixed

--- writings/script.js
+++ writings/script.js
@@ -1,6 +1,6 @@
 ﻿(function() {
 
-    var index = {};
+    var query = parseQuery(location.search);
 
     fetch("Pages.txt").then(function(t) {
         list(document.getElementById("pages"), t.split(/\r\n|[\r\n]/), "Pages");
@@ -11,21 +11,18 @@
     });
 
 
-
 //----------------------------------------------------------------------------------------------------
 function selectPage() {
 
-    var query = parseQuery(location.search);
-
     if(query) {
-        if(query["page"]) {
-            var id = "Pages/" + query["page"];
+        if(query.params["page"]) {
+            var id = "Pages/" + query.params["page"];
             if(document.getElementById(id)) {
                 show(id);
                 return;
             }
-        } else if(query["tag"]) {
-            var id = "Tags/" + query["page"];
+        } else if(query.params["tag"]) {
+            var id = "Tags/" + query.params["page"];
             if(document.getElementById(id)) {
                 show(id);
                 return; 
@@ -41,28 +38,6 @@ function selectPage() {
         }
     }
 
-
-}
-//----------------------------------------------------------------------------------------------------
-function parseQuery(t) {
-
-    var q = {};
-
-    if(typeof t != "string") {
-        return null;
-    }
-
-    t = t.replace(/^\?/, "").split("&");
-
-    for(var i = 0; i < t.length; i++) {
-        var e = t[i].split("=");
-        var p = trim(e[0] || "");
-        var v = trim(e[1] || "");
-        q[p] = v;
-    }
-
-    return q;
-
 }
 //----------------------------------------------------------------------------------------------------
 function list(u, a, h) {
@@ -72,11 +47,13 @@ function list(u, a, h) {
     var t = [];
 
     t.push('<h3 ref="' + html(h) + '">' + html(h) + '</h3>');
+
     t.push('<ul id="' + html(h) + '">');
     for(var i = 0; i < a.length; i++) {
         var e = a[i];
-        v = e.split("/").pop();
-        t.push('<li id="' + html(e) + '">' + html(v) + '</li>\r\n');
+        var id = e.split("\t")[0];
+        var name = e.split("\t")[1];
+        t.push('<li id="' + html(id) + '">' + html(name) + '</li>\r\n');
     }
     t.push('</ul>');
 
@@ -85,18 +62,22 @@ function list(u, a, h) {
     var e = u.getElementsByTagName("li");
     for(var i = 0; i < e.length; i++) {
         e[i].addEventListener("click", function() {
-            show(this.getAttribute("id"));
+            var id = this.getAttribute("id");
+            show(id);
         }, false);
     }
 
     var e = u.getElementsByTagName("h3");
     for(var i = 0; i < e.length; i++) {
         e[i].addEventListener("click", function() {
-            var ul = document.getElementById(this.getAttribute("ref"));
-            if(ul && ul.style.display != "block") {
-                ul.style.display = "block";
-            } else {
-                ul.style.display = "none";
+            var style = document.defaultView.getComputedStyle(this, "cursor");
+            if(style.cursor == "pointer") {
+                var ul = document.getElementById(this.getAttribute("ref"));
+                if(ul.style.display != "block") {
+                    ul.style.display = "block";
+                } else {
+                    ul.style.display = "none";
+                }
             }
         }, false);
     }
@@ -104,29 +85,21 @@ function list(u, a, h) {
 
 }
 //----------------------------------------------------------------------------------------------------
-function page(u, a, h) {
+function page(u, a, id) {
 
-    var hash = location.hash;
-
-    var path = h.split("/");
-    var page = path.pop();
-    path = path == "Tags" ? "tag" : "page";
-
-    history.replaceState("", "", "?" + html(path) + "=" + html(page));
-
+    var e = document.getElementById(id);
+    var h = e ? e.innerText : "";
 
     var t = [];
+
     t.push('<h2>' + html(h) + '</h2>');
 
     for(var i = 0; i < a.length; i++) {
         var e = a[i].split("\t");
-        var id = e[0];
-        var date = e[1];
-        var title = e[2];
-        t.push('<section class="fold" id="' + html(id) + '">');
-        t.push('<h3>' + html(title) + '</h3>\r\n');
-        t.push('<div class="entry" id="' + html(id) + '-entry"></div>\r\n');
-        t.push('<div class="date">' + html(date) + '</div>\r\n');
+        t.push('<section class="fold" id="' + html(e[0]) + '">');
+        t.push('<h3>' + html(e[2]) + '</h3>\r\n');
+        t.push('<div class="entry" id="' + html(e[0]) + '-entry"></div>\r\n');
+        t.push('<div class="date">' + html(e[1]) + '</div>\r\n');
         if(i > 5) {
             t.push('<div class="top">Back to top</div>\r\n');
         }
@@ -153,13 +126,22 @@ function page(u, a, h) {
     }
 
 
-    var id = hash.replace(/^#/, "");
-    if(id && /^[0-9a-f]{32}$/i.test(id)) {
-        var e = document.getElementById(id);
+    var page_id = id.split("/").pop();
+    var page_type = /Tags/.test(id) ? "tag" : "page";
+    var section_id = query.params["id"];
+
+    if(section_id) {
+        history.replaceState("", "", "?" + html(page_type) + "=" + html(page_id) + "&id=" + html(section_id));
+    } else {
+        history.replaceState("", "", "?" + html(page_type) + "=" + html(page_id));
+    }
+
+    if(section_id && /^[0-9a-f]{32}$/i.test(section_id)) {
+        var e = document.getElementById(section_id);
         if(e) {
             e.scrollIntoView();
             if(e.className == "fold") {
-                unfold(id);
+                unfold(section_id);
             }
         }
     }
@@ -175,17 +157,21 @@ function unfold(id) {
         var e = document.getElementById(id + "-entry");
         e.innerHTML = html(t, true);
         e.parentNode.className = "";
-        location.hash = id;
+
+        var q = parseQuery(location.search);
+        q.params["id"] = id;
+        var p = q.join();
+        history.replaceState("", "", "?" + p);
     });
 
 }
 //----------------------------------------------------------------------------------------------------
-function show(path) {
+function show(id) {
 
-    var url = path + ".txt";
+    var url = id + ".txt";
 
     fetch(url).then(function(t, options) {
-        page(document.getElementById("content"), t.split(/\r\n|[\r\n]/), path);
+        page(document.getElementById("content"), t.split(/\r\n|[\r\n]/), id);
     });
 
 }
@@ -209,6 +195,39 @@ function fetch(url) {
 
         request.send();
     });
+
+}
+//----------------------------------------------------------------------------------------------------
+function parseQuery(t) {
+
+    var query = {};
+    var params = {};
+
+    if(typeof t != "string") {
+        return null;
+    }
+
+    t = t.replace(/^\?/, "").split("&");
+
+    for(var i = 0; i < t.length; i++) {
+        var e = t[i].split("=");
+        var p = trim(e[0] || "");
+        var v = trim(e[1] || "");
+        params[p] = v;
+    }
+
+    query.params = params;
+
+    query.join = function() {
+        var t = [];
+        for(var p in params) {
+            t.push(p + "=" + params[p]);
+        }
+        return t.join("&");
+    };   
+
+
+    return query;
 
 }
 //----------------------------------------------------------------------------------------------------
